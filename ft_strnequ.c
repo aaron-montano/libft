0e1f40abec518d2330ab3fdf46108e939a9ef4db
Aaron Montano <amontano@e1z3r6p2.42.us.org>
2018-04-25 19:24:37 -0700

    round3

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: amontano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 21:25:03 by amontano          #+#    #+#             */
-/*   Updated: 2018/04/25 00:13:41 by amontano         ###   ########.fr       */
+/*   Updated: 2018/04/25 19:22:36 by amontano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
-		i++;
-	if (s1[i] == s2[i])
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	if (ft_strncmp(s1, s2, n) == 0)
 		return (1);
 	else
 		return (0);
